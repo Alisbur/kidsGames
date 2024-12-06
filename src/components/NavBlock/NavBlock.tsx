214e7ca789658c6a468ca3ddbcb3826499e53a4a
Alisbur <mail-4-eugen@yandex.ru>
2024-12-07 00:55:49 +0300

    fix: fixes last part bug / hover effect

--- src/components/NavBlock/NavBlock.tsx
+++ src/components/NavBlock/NavBlock.tsx
@@ -1,16 +1,21 @@
 import styles from './NavBlock.module.scss'
 import { NavigateButton } from '../../shared/ui/navigate-button/navigate-button'
+import { ReactNode } from 'react';
 
 type NavBlockProps = {
   onNext: ()=>void;
   onPrev: ()=>void;
+  nextDisabled?: boolean;
+  prevDisabled?: boolean;
+  children?: JSX.Element;
 }
 
-export function NavBlock({onNext, onPrev}: NavBlockProps) {
+export function NavBlock({onNext, onPrev, nextDisabled = false, prevDisabled = false, children}: NavBlockProps) {
   return (
     <div className={styles.container}>
-      <NavigateButton type="back" onClick={onPrev} />
-      <NavigateButton type="forward" onClick={onNext} />
+      <NavigateButton type="back" onClick={onPrev} disabled={prevDisabled} />
+      {children}
+      <NavigateButton type="forward" onClick={onNext} disabled={nextDisabled}/>
     </div>
   )
 }
